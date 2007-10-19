f74769d3f64d3bf43c294487127abcdc53d39226
Llewellyn Pritchard <xacc.ide@gmail.com>
2007-10-19 23:19:13 +0000

    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS02]$/IronScheme;C6596

--- IronScheme/IronSchemeConsole/test.scm
+++ IronScheme/IronSchemeConsole/test.scm
@@ -2,8 +2,16 @@
 
 
 (define let (macro (args . body) 
-  `((lambda ,(map first args) ,@body) ,@(map second args))))
+  (if (symbol? args)
+    ((lambda (name args body)
+      `((lambda (,name)
+        (set! ,name (lambda ,(map first args) ,@body))
+        (,name ,@(map second args))) #f))
+        args (car body) (cdr body))
+    `((lambda ,(map first args) ,@body) ,@(map second args)))))
 
+    
+    
 (define define-macro (macro (nargs . body)
   (let ((name (car nargs))
         (args (cdr nargs)))
@@ -16,7 +24,12 @@
 (foo 1 2 3 4)
 )
 
+(newline)
 
+(let bar ((a 1))
+  (if (null? a)
+    (display "eureka!")
+    (bar '())))
 
 
 
