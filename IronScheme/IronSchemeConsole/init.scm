7a9b444199a61f4835c3fa12cd1b259243dfae63
leppie <xacc.ide@gmail.com>
2007-10-23 21:18:37 +0000

    git-svn-id: https://ironscheme.svn.codeplex.com/svn@6783 2327b42d-5241-43d6-9e2a-de5ac946f064

--- IronScheme/IronSchemeConsole/init.scm
+++ IronScheme/IronSchemeConsole/init.scm
@@ -6,6 +6,7 @@
 
 ;; this wil call load immediately, use with care :p
 ;; this is usefull for loading references at compile-time
+;; there is a side-effect running this in the interpreter, it will be dispatched twice
 (define load-diags
   (macro () `,(load "IronScheme.Diagnostics.dll")))
   
