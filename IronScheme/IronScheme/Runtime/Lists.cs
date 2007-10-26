0c0ed99f3de85888f1a4aaae7adaf731db5f3520
Llewellyn Pritchard <xacc.ide@gmail.com>
2007-10-26 20:04:53 +0000

    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS02]$/IronScheme;C6893

--- IronScheme/IronScheme/Runtime/Lists.cs
+++ IronScheme/IronScheme/Runtime/Lists.cs
@@ -2,10 +2,10 @@
 /* ****************************************************************************
  * Copyright (c) Llewellyn Pritchard. 
  *
- * This source code is subject to terms and conditions of the Microsoft Permissive License. 
+ * This source code is subject to terms and conditions of the Microsoft Public License. 
  * A copy of the license can be found in the License.html file at the root of this distribution. 
  * By using this source code in any fashion, you are agreeing to be bound by the terms of the 
- * Microsoft Permissive License.
+ * Microsoft Public License.
  *
  * You must not remove this notice, or any other, from this software.
  * ***************************************************************************/
