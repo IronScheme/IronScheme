be35a7e8ef5795aa70993a1adcef18c0d69b75ed
leppie <xacc.ide@gmail.com>
2008-05-22 17:44:29 +0000

    VSTS going funny...
    git-svn-id: https://ironscheme.svn.codeplex.com/svn@11432 2327b42d-5241-43d6-9e2a-de5ac946f064

--- IronScheme/IronSchemeConsole/srfi/31.scm
+++
@@ -1,8 +0,0 @@
-
-(define-syntax rec
-  (syntax-rules ()
-    ((rec (NAME . VARIABLES) . BODY)
-     (letrec ( (NAME (lambda VARIABLES . BODY)) ) NAME))
-    ((rec NAME EXPRESSION)
-     (letrec ( (NAME EXPRESSION) ) NAME))))
-
--- IronScheme/IronSchemeConsole/srfi/8.scm
+++
@@ -1,7 +0,0 @@
-
-
-(define-syntax receive
-  (syntax-rules ()
-    ((receive formals expression body ...)
-     (call-with-values (lambda () expression)
-                       (lambda formals body ...)))))
