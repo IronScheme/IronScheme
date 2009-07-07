927a2a0d258efaf26ce0511ed2c30837d6b21204
leppie <xacc.ide@gmail.com>
2009-07-07 20:02:19 +0000

    Fixed an old bug, that was only discovered now. Files without a preamble was opening in UTF8 by default, where it should have been Latin-1.
    git-svn-id: https://ironscheme.svn.codeplex.com/svn@28126 2327b42d-5241-43d6-9e2a-de5ac946f064

--- IronScheme/IronScheme.Console/srfi/25/array.scm
+++ IronScheme/IronScheme.Console/srfi/25/array.scm
@@ -1,4 +1,4 @@
-﻿;;; array
+;;; array
 ;;; 1997 - 2001 Jussi Piitulainen
 
 ;;; --- Intro ---
@@ -333,7 +333,7 @@
 
 ;;; (array:good-shape? shape)
 ;;; returns true if `shape' is an array of the right shape and its
-;;; elements are exact integers that pairwise bound intervals `[lo..hi)´.
+;;; elements are exact integers that pairwise bound intervals `[lo..hi)�.
 
 (define (array:good-shape? shape)
   (and (array:array? shape)
