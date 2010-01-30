d3f238a896348b3373e42ed18c78f43d259aa97d
leppie <xacc.ide@gmail.com>
2010-01-30 01:05:17 +0000

    git-svn-id: https://ironscheme.svn.codeplex.com/svn@38570 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ IronScheme/IronScheme.Console/ilmerge.cmd
@@ -0,0 +1,1 @@
+"C:\Program Files\Microsoft\ILMerge\ILMerge.exe"/ndebug /out:isc.exe IronScheme.Console.exe IronScheme.dll IronScheme.Closures.dll Microsoft.Scripting.dll Oyster.IntX.dll ironscheme.boot.dll 
