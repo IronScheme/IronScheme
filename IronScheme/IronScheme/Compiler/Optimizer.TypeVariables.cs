859e48b6e38b61433196c8cc85619bdd6391cddd
leppie <xacc.ide@gmail.com>
2011-05-21 16:52:17 +0000

    Added optimization pass to type local variables (mostly just for Callable). Updated bootfile.
    git-svn-id: https://ironscheme.svn.codeplex.com/svn@74017 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ IronScheme/IronScheme/Compiler/Optimizer.TypeVariables.cs
@@ -0,0 +1,48 @@
+﻿#region License
+/* Copyright (c) 2007,2008,2009,2010,2011 Llewellyn Pritchard 
+ * All rights reserved.
+ * This source code is subject to terms and conditions of the BSD License.
+ * See docs/license.txt. */
+#endregion
+
+using Microsoft.Scripting.Ast;
+using System;
+
+namespace IronScheme.Compiler
+{
+  static partial class Optimizer
+  {
+    class TypeVariables : OptimizerBase
+    {
+      public override void Optimize()
+      {
+        Pass0 p0 = new Pass0();
+        p0.WalkNode(Root);
+      }
+
+      class Pass0 : DeepWalker
+      {
+        protected override void PostWalk(WriteStatement node)
+        {
+          var v = node.Variable;
+          if (v.Kind != Variable.VariableKind.Global)
+          {
+            var val = node.Value;
+            if (v.Type == typeof(object))
+            {
+              if (val.Type != typeof(object))
+              {
+                v.Type = val.Type;
+              }
+            }
+            else if (v.Type != val.Type)
+            {
+              v.Type = typeof(object);
+            }
+          }
+          base.PostWalk(node);
+        }
+      }
+    }
+  }
+}
