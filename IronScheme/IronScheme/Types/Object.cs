7e9463eae6dc937bb59d294a8167e7bed92080df
Llewellyn Pritchard <xacc.ide@gmail.com>
2011-06-21 18:09:04 +0000

    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS02]$/IronScheme;C75903

---
+++ IronScheme/IronScheme/Types/Object.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IronScheme.Types
+{
+  public class Object
+  {
+  }
+
+  public class GenericObject<T> : Object
+  {
+  }
+}
