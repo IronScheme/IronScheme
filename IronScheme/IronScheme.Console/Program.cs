28b3c65267c6c4d97f7c1cd82b30514cc2b84cfd
Llewellyn Pritchard <xacc.ide@gmail.com>
2011-08-24 15:03:40 +0000

    Added some checks for profiler dll in application directory.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS02]$/IronScheme;C78703

--- IronScheme/IronScheme.Console/Program.cs
+++ IronScheme/IronScheme.Console/Program.cs
@@ -22,89 +22,110 @@ namespace IronScheme.Runtime
       {
         const string PROFILER_GUID = "{9E2B38F2-7355-4C61-A54F-434B7AC266C0}";
 
-        ExecuteRegsvr32(false, true);
-
-        ProcessStartInfo psi;
-
-        // create a process executor
-        psi = new ProcessStartInfo(typeof(Program).Assembly.Location);
-
-        // ----- SET THE CLR ENVIRONMENT VARIABLES -------------------
-
-        // set the COR_PROFILER env var. This indicates to the CLR which COM object to
-        // instantiate for profiling.
-        if (psi.EnvironmentVariables.ContainsKey("COR_PROFILER") == true)
-          psi.EnvironmentVariables["COR_PROFILER"] = PROFILER_GUID;
-        else
-          psi.EnvironmentVariables.Add("COR_PROFILER", PROFILER_GUID);
-
-        ////// set the Cor_Enable_Profiling env var. This indicates to the CLR whether or
-        ////// not we are using the profiler at all.  1 = yes, 0 = no.
-        if (psi.EnvironmentVariables.ContainsKey("COR_ENABLE_PROFILING") == true)
-          psi.EnvironmentVariables["COR_ENABLE_PROFILING"] = "1";
-        else
-          psi.EnvironmentVariables.Add("COR_ENABLE_PROFILING", "1");
-
-        // ----- RUN THE PROCESS -------------------
-
-
-        string[] argargs = new string[args.Length - 1];
-        Array.Copy(args, 1, argargs, 0, argargs.Length);
-
-        psi.Arguments = string.Join(" ", argargs);
-        psi.UseShellExecute = false;
-
-        try
+        if (ExecuteRegsvr32(false, true))
         {
-          Process p = Process.Start(psi);
-          //p.PriorityBoostEnabled = true;
+          ProcessStartInfo psi;
 
-          p.WaitForExit();
+          // create a process executor
+          psi = new ProcessStartInfo(typeof(Program).Assembly.Location);
 
-          return p.ExitCode;
+          // ----- SET THE CLR ENVIRONMENT VARIABLES -------------------
+
+          // set the COR_PROFILER env var. This indicates to the CLR which COM object to
+          // instantiate for profiling.
+          if (psi.EnvironmentVariables.ContainsKey("COR_PROFILER") == true)
+            psi.EnvironmentVariables["COR_PROFILER"] = PROFILER_GUID;
+          else
+            psi.EnvironmentVariables.Add("COR_PROFILER", PROFILER_GUID);
+
+          ////// set the Cor_Enable_Profiling env var. This indicates to the CLR whether or
+          ////// not we are using the profiler at all.  1 = yes, 0 = no.
+          if (psi.EnvironmentVariables.ContainsKey("COR_ENABLE_PROFILING") == true)
+            psi.EnvironmentVariables["COR_ENABLE_PROFILING"] = "1";
+          else
+            psi.EnvironmentVariables.Add("COR_ENABLE_PROFILING", "1");
+
+          // ----- RUN THE PROCESS -------------------
+
+
+          string[] argargs = new string[args.Length - 1];
+          Array.Copy(args, 1, argargs, 0, argargs.Length);
+
+          psi.Arguments = string.Join(" ", argargs);
+          psi.UseShellExecute = false;
+
+          try
+          {
+            Process p = Process.Start(psi);
+            //p.PriorityBoostEnabled = true;
+
+            p.WaitForExit();
+
+            return p.ExitCode;
+          }
+          finally
+          {
+            ExecuteRegsvr32(false, false);
+          }
         }
-        finally
+        else
         {
-          ExecuteRegsvr32(false, false);
+          // remove -profile
+          string[] argargs = new string[args.Length - 1];
+          Array.Copy(args, 1, argargs, 0, argargs.Length);
+
+          args = argargs;
         }
       }
-      else
+
+      if (Array.IndexOf(args, "--remoting-server") >= 0)
       {
-        if (Array.IndexOf(args, "--remoting-server") >= 0)
-        {
-          Host.Start();
-          args = Array.FindAll(args, x => x != "--remoting-server");
-        }
-        //Encoding oi = Console.InputEncoding;
-        Encoding oo = Console.OutputEncoding;
-
-        //Debugger.Launch();
-
-        try
-        {
-          //Console.InputEncoding = Encoding.UTF8;
-            Console.OutputEncoding = Encoding.UTF8;
-          return new IronSchemeConsoleHost().Run(args);
-        }
-        finally
-        {
-          Console.OutputEncoding = oo;
-          //Console.InputEncoding = oi;
-        }
+        Host.Start();
+        args = Array.FindAll(args, x => x != "--remoting-server");
       }
+      //Encoding oi = Console.InputEncoding;
+      Encoding oo = Console.OutputEncoding;
+
+      //Debugger.Launch();
+
+      try
+      {
+        //Console.InputEncoding = Encoding.UTF8;
+        Console.OutputEncoding = Encoding.UTF8;
+        return new IronSchemeConsoleHost().Run(args);
+      }
+      finally
+      {
+        Console.OutputEncoding = oo;
+        //Console.InputEncoding = oi;
+      }
+
     }
 
     // https://github.com/sawilde/opencover/blob/master/main/OpenCover.Framework/ProfilerRegistration.cs
-    static void ExecuteRegsvr32(bool userRegistration, bool register)
+    static bool ExecuteRegsvr32(bool userRegistration, bool register)
     {
       const string UserRegistrationString = "/n /i:user";
 
+      var ppath = GetProfilerPath();
+
+      if (!File.Exists(ppath))
+      {
+        if (register)
+        {
+          Console.WriteLine("WARNING: Profiler DLL ({0}) not available in application directory. Profiling will be disabled.", Path.GetFileName(ppath));
+        }
+
+        return false;
+      }
+
       var startInfo = new ProcessStartInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), "regsvr32.exe"),
                                string.Format("/s {2} {0} \"{1}\"", userRegistration ? UserRegistrationString : String.Empty,
-                               GetProfilerPath(), register ? string.Empty : "/u")) { CreateNoWindow = true };
+                               ppath, register ? string.Empty : "/u")) { CreateNoWindow = true };
 
       var process = Process.Start(startInfo);
       process.WaitForExit();
+      return true;
     }
 
     static string GetProfilerPath()
