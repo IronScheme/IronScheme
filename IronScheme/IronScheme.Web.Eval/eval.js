e86731b99e9657d21bd49e623edf3b088ea5ddf5
leppie <xacc.ide@gmail.com>
2011-09-08 19:48:08 +0000

    Prettified eval web. Fixed a few bugs in eval web.
    git-svn-id: https://ironscheme.svn.codeplex.com/svn@79708 2327b42d-5241-43d6-9e2a-de5ac946f064

--- IronScheme/IronScheme.Web.Eval/eval.js
+++ IronScheme/IronScheme.Web.Eval/eval.js
@@ -1,7 +1,22 @@
-$("#eform").submit(function(event) {
-  event.preventDefault();
-  $.post('', { expr: $("#expr").val() },
-      function(data) {
+function docall() {
+  $.post('/', { expr: $("#expr").val() },
+      function (data) {
         $("#result").html(data);
       });
+  return false;
+}
+
+var editor = CodeMirror.fromTextArea(document.getElementById("expr"),
+                                      { lineNumbers: true, 
+                                        matchBrackets: true,
+                                        tabMode: "shift",
+                                        indentUnit: 2 });
+
+$("#eform").submit(function (event) {
+  editor.save();
+  $("#result").text('Please wait...');
+  event.preventDefault();
+  return docall();
 });
+
+
