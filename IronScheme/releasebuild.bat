deb060afcc55046bd147fc042f6dba333901e068
leppie <xacc.ide@gmail.com>
2011-11-10 15:57:43 +0000

    Make build batch file nicer.
    git-svn-id: https://ironscheme.svn.codeplex.com/svn@82626 2327b42d-5241-43d6-9e2a-de5ac946f064

--- IronScheme/releasebuild.bat
+++ IronScheme/releasebuild.bat
@@ -1,4 +1,4 @@
-%windir%\microsoft.net\framework\v3.5\msbuild IronScheme2008.sln /p:Configuration=Release "/p:Platform=Any CPU"
+%windir%\microsoft.net\framework\v3.5\msbuild /nologo /v:m /m IronScheme2008.sln /p:Configuration=Release
 @IF %ERRORLEVEL% NEQ 0 GOTO err
 @exit /B 0
 :err
