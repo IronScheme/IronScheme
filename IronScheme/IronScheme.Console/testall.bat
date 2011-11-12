6220b156c1808655e447f0b903b7c36b80ad6b4c
Llewellyn Pritchard <xacc.ide@gmail.com>
2011-11-12 18:18:42 +0000

    Add peverify for debug build too.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS02]$/IronScheme;C82725

--- IronScheme/IronScheme.Console/testall.bat
+++ IronScheme/IronScheme.Console/testall.bat
@@ -1,10 +1,11 @@
 call isc ironscheme-buildscript.ss
 call isc ironscheme-buildscript.ss
-peverify ironscheme.boot.dll
+peverify /nologo ironscheme.boot.dll
 call isc compile-system-libraries.sps
 call isc compile-system-libraries.sps
 call isc -debug ironscheme-buildscript.ss
 call isc -debug ironscheme-buildscript.ss
+peverify /nologo /ignore=0x80131820 ironscheme.boot.dll
 call isc -debug compile-system-libraries.sps
 call isc -debug compile-system-libraries.sps
 call isc ironscheme-buildscript.ss
