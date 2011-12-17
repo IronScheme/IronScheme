33246aa5799cc6aba037f65a683f6936a7786d53
Llewellyn Pritchard <xacc.ide@gmail.com>
2011-12-17 19:38:32 +0000

    Use a 'random' filename for embedded resource.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS02]$/IronScheme;C83960

--- IronScheme/IronScheme/ExecutableTemplate.cs
+++ IronScheme/IronScheme/ExecutableTemplate.cs
@@ -7,7 +7,7 @@ namespace IronScheme
   {
     static void Main(string[] args)
     {
-      using (TextReader r = new StreamReader(typeof(ExecutableTemplate).Assembly.GetManifestResourceStream("program.sps")))
+      using (TextReader r = new StreamReader(typeof(ExecutableTemplate).Assembly.GetManifestResourceStream("AA853EBC-97FA-4e82-86FD-749009FDDE5D.sps")))
       {
         Cons cmdline = Cons.FromList(args);
         RuntimeExtensions.Eval("(apply load-port {0} {1})", r, cmdline);
