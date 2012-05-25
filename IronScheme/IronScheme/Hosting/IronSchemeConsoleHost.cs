1678cb6fbae9548fe4d987b31201f0e98a31a18b
Llewellyn Pritchard <xacc.ide@gmail.com>
2012-05-25 18:38:14 +0000

    Add VS2010/11 plugin binary. Update installer.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS02]$/IronScheme;C90974

--- IronScheme/IronScheme/Hosting/IronSchemeConsoleHost.cs
+++ IronScheme/IronScheme/Hosting/IronSchemeConsoleHost.cs
@@ -15,7 +15,7 @@ namespace IronScheme.Hosting
 {
   public sealed class IronSchemeConsoleHost : ConsoleHost
   {
-    const string VERSION = "1.0 RC6";
+    const string VERSION = "1.0 RC7";
     string logo;
     public IronSchemeConsoleHost()
     {
