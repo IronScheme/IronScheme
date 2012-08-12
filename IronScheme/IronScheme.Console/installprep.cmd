97edb5ef96a00f44b04d65542c15ab7769f41703
Llewellyn Pritchard <xacc.ide@gmail.com>
2012-08-12 15:58:04 +0000

    Add some more scripts.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS02]$/IronScheme;C93714

---
+++ IronScheme/IronScheme.Console/installprep.cmd
@@ -0,0 +1,4 @@
+call ilmerge-dll.cmd
+..\..\..\tools\ReferenceRemover IronScheme.Web.Runtime.dll "IronScheme\..*|Oyster.IntX.*" IronScheme.dll
+..\..\..\tools\ReferenceRemover IronScheme.Remoting.Server.dll "IronScheme\..*|Oyster.IntX.*" "IronScheme.Remoting.*" IronScheme.dll
+call genv4.cmd
---
+++ IronScheme/IronScheme.Console/ngen-all.cmd
@@ -0,0 +1,4 @@
+%windir%\Microsoft.NET\Framework64\v2.0.50727\ngen.exe install IronScheme.Console.exe
+%windir%\Microsoft.NET\Framework\v2.0.50727\ngen.exe install IronScheme.Console32.exe
+%windir%\Microsoft.NET\Framework64\v4.0.30319\ngen.exe install IronScheme.Console-v4.exe
+%windir%\Microsoft.NET\Framework\v4.0.30319\ngen.exe install IronScheme.Console32-v4.exe
