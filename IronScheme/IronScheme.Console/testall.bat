81680b00cb709ee309a3d6ecf9c4da352026a680
Llewellyn Pritchard <xacc.ide@gmail.com>
2012-10-27 17:19:25 +0000

    fix testall script.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS02]$/IronScheme;C95646

--- IronScheme/IronScheme.Console/testall.bat
+++ IronScheme/IronScheme.Console/testall.bat
@@ -1,18 +1,18 @@
-call isc ironscheme-buildscript.ss
+call isc ironscheme-buildscript.sps
 @IF %ERRORLEVEL% NEQ 0 exit /b 1
-call isc ironscheme-buildscript.ss
+call isc ironscheme-buildscript.sps
 peverify /nologo ironscheme.boot.dll
 @IF %ERRORLEVEL% NEQ 0 exit /b 1
 call isc compile-system-libraries.sps
 call isc compile-system-libraries.sps
-call isc -debug ironscheme-buildscript.ss
+call isc -debug ironscheme-buildscript.sps
 @IF %ERRORLEVEL% NEQ 0 exit /b 1
-call isc -debug ironscheme-buildscript.ss
+call isc -debug ironscheme-buildscript.sps
 peverify /nologo /ignore=0x80131820 ironscheme.boot.dll
 @IF %ERRORLEVEL% NEQ 0 exit /b 1
 call isc -debug compile-system-libraries.sps
 call isc -debug compile-system-libraries.sps
-call isc ironscheme-buildscript.ss
+call isc ironscheme-buildscript.sps
 call isc compile-system-libraries.sps
 call run-tests.bat
 call srfitest.bat
