6495ff53c62c4160ab3a97b7573652798e124cad
leppie <xacc.ide@gmail.com>
2013-04-12 13:53:01 +0000

    Added NamespaceRenamer tool. Update install-prep.cmd to make included namespaces 'IronSchemey' (prevents type clashes from DLR/Oyster/gppg, only IronScheme namespace is seen). Added common Cecil dependency for above and ReferenceRemover tool.
    git-svn-id: https://ironscheme.svn.codeplex.com/svn@99904 2327b42d-5241-43d6-9e2a-de5ac946f064

--- IronScheme/IronScheme.Console/installprep.cmd
+++ IronScheme/IronScheme.Console/installprep.cmd
@@ -1,5 +1,7 @@
 call ilmerge-dll.cmd
+..\..\..\tools\NamespaceRenamer IronScheme.dll Microsoft=IronScheme Oyster.Math=IronScheme.Scripting.Math gppg=IronScheme.gppg
 ..\..\..\tools\ReferenceRemover IronScheme.Web.Runtime.dll "IronScheme\..*|Oyster.IntX.*" IronScheme.dll
 ..\..\..\tools\ReferenceRemover IronScheme.Remoting.Server.dll "IronScheme\..*|Oyster.IntX.*" "IronScheme.Remoting.*" IronScheme.dll
+peverify /nologo /ignore=0x80131820,0x801318DE,0x80131854,0x8013185D,0x80131228 IronScheme.dll
 call genv4.cmd
 call stage.cmd
