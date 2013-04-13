9e2c05aa1f2af25455d80b6106d1f6a7c96d5cfb
Llewellyn Pritchard <xacc.ide@gmail.com>
2013-04-13 17:32:03 +0000

    Updated NamespaceRenamer tool. Fix transformations.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS02]$/IronScheme;C99952

--- IronScheme/IronScheme.Console/installprep.cmd
+++ IronScheme/IronScheme.Console/installprep.cmd
@@ -2,6 +2,13 @@ call ilmerge-dll.cmd
 ..\..\..\tools\NamespaceRenamer IronScheme.dll Microsoft=IronScheme Oyster.Math=IronScheme.Scripting.Math gppg=IronScheme.gppg
 ..\..\..\tools\ReferenceRemover IronScheme.Web.Runtime.dll "IronScheme\..*|Oyster.IntX.*" IronScheme.dll
 ..\..\..\tools\ReferenceRemover IronScheme.Remoting.Server.dll "IronScheme\..*|Oyster.IntX.*" "IronScheme.Remoting.*" IronScheme.dll
+..\..\..\tools\NamespaceRenamer -r IronScheme.Web.Runtime.dll Microsoft=IronScheme
+..\..\..\tools\NamespaceRenamer -r IronScheme.Remoting.Server.dll Microsoft=IronScheme
 peverify /nologo /ignore=0x80131820,0x801318DE,0x80131854,0x8013185D,0x80131228 IronScheme.dll
+@IF %ERRORLEVEL% NEQ 0 exit /b 1
+peverify /nologo IronScheme.Web.Runtime.dll
+@IF %ERRORLEVEL% NEQ 0 exit /b 1
+peverify /nologo IronScheme.Remoting.Server.dll
+@IF %ERRORLEVEL% NEQ 0 exit /b 1
 call genv4.cmd
 call stage.cmd
