65458acef98e84978813975afdaa376c650c7611
Llewellyn Pritchard <xacc.ide@gmail.com>
2013-04-14 11:45:12 +0000

    Update NamespaceRenamer tool Add more error checking in installprep.cmd
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS02]$/IronScheme;C99970

--- IronScheme/IronScheme.Console/installprep.cmd
+++ IronScheme/IronScheme.Console/installprep.cmd
@@ -1,4 +1,4 @@
-call ilmerge-dll.cmd
+@call ilmerge-dll.cmd
 ..\..\..\tools\NamespaceRenamer IronScheme.dll Microsoft=IronScheme Oyster.Math=IronScheme.Scripting.Math gppg=IronScheme.gppg
 ..\..\..\tools\ReferenceRemover IronScheme.Web.Runtime.dll "IronScheme\..*|Oyster.IntX.*" IronScheme.dll
 ..\..\..\tools\ReferenceRemover IronScheme.Remoting.Server.dll "IronScheme\..*|Oyster.IntX.*" "IronScheme.Remoting.*" IronScheme.dll
@@ -10,5 +10,5 @@ peverify /nologo IronScheme.Web.Runtime.dll
 @IF %ERRORLEVEL% NEQ 0 exit /b 1
 peverify /nologo IronScheme.Remoting.Server.dll
 @IF %ERRORLEVEL% NEQ 0 exit /b 1
-call genv4.cmd
-call stage.cmd
+@call genv4.cmd
+@call stage.cmd
