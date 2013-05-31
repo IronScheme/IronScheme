ebedf2e93b6a461510a182d7ef9118fbe7cd956a
Llewellyn Pritchard <xacc.ide@gmail.com>
2013-05-31 14:22:15 +0000

    Update test runners. Add minikanren tests.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS02]$/IronScheme;C100775

--- IronScheme/IronScheme.Console/testall.bat
+++ IronScheme/IronScheme.Console/testall.bat
@@ -1,19 +1,21 @@
-call isc ironscheme-buildscript.sps
+isc ironscheme-buildscript.sps
 @IF %ERRORLEVEL% NEQ 0 exit /b 1
-call isc ironscheme-buildscript.sps
+isc ironscheme-buildscript.sps
 peverify /nologo ironscheme.boot.dll
 @IF %ERRORLEVEL% NEQ 0 exit /b 1
-call isc compile-system-libraries.sps
-call isc compile-system-libraries.sps
-call isc -debug ironscheme-buildscript.sps
+isc compile-system-libraries.sps
+isc compile-system-libraries.sps
+isc -debug ironscheme-buildscript.sps
 @IF %ERRORLEVEL% NEQ 0 exit /b 1
-call isc -debug ironscheme-buildscript.sps
+isc -debug ironscheme-buildscript.sps
 peverify /nologo /ignore=0x80131820 ironscheme.boot.dll
 @IF %ERRORLEVEL% NEQ 0 exit /b 1
-call isc -debug compile-system-libraries.sps
-call isc -debug compile-system-libraries.sps
-call isc ironscheme-buildscript.sps
-call isc compile-system-libraries.sps
+isc -debug compile-system-libraries.sps
+isc -debug compile-system-libraries.sps
+isc ironscheme-buildscript.sps
+isc compile-system-libraries.sps
 call run-tests.bat
 call srfitest.bat
-call isc lib\pfds\tests.scm > pfds.log
+isc lib\pfds\tests.scm > pfds.log
+echo (include "lib/minikanren/mktests.scm") | isc > mktest.log
+
