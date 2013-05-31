66207d40db9fb40096971abf1c1902346f0cf8e5
leppie <xacc.ide@gmail.com>
2013-05-31 14:22:15 +0000

    Update test runners. Add minikanren tests.
    git-svn-id: https://ironscheme.svn.codeplex.com/svn@100775 2327b42d-5241-43d6-9e2a-de5ac946f064

--- IronScheme/IronScheme.Console/run-tests.bat
+++ IronScheme/IronScheme.Console/run-tests.bat
@@ -1,3 +1,3 @@
-call isc tests\r6rs\run.sps > tests.log
-call isc tests\trigtest.sps >> tests.log
-call isc tests\clisp-number-tests.sps >> tests.log
+isc tests\r6rs\run.sps > tests.log
+isc tests\trigtest.sps >> tests.log
+isc tests\clisp-number-tests.sps >> tests.log
--- IronScheme/IronScheme.Console/srfitest.bat
+++ IronScheme/IronScheme.Console/srfitest.bat
@@ -1,20 +1,20 @@
 echo SRFI tests > srfitest.log
-call isc lib\srfi\tests\and-let_.sps >> srfitest.log
-call isc lib\srfi\tests\compare-procedures.sps >> srfitest.log
-call isc lib\srfi\tests\cut.sps >> srfitest.log
-call isc lib\srfi\tests\eager-comprehensions.sps >> srfitest.log
-call isc lib\srfi\tests\intermediate-format-strings.sps >> srfitest.log
-call isc lib\srfi\tests\lightweight-testing.sps >> srfitest.log
-call isc lib\srfi\tests\lists.sps >> srfitest.log
-call isc lib\srfi\tests\multi-dimensional-arrays--arlib.sps >> srfitest.log
-call isc lib\srfi\tests\multi-dimensional-arrays.sps >> srfitest.log
-call isc lib\srfi\tests\os-environment-variables.sps >> srfitest.log
-call isc lib\srfi\tests\procedure-arity.sps >> srfitest.log
-call isc lib\srfi\tests\print-ascii.sps >> srfitest.log
-rem call isc lib\srfi\tests\random-conftest.sps >> srfitest.log
-call isc lib\srfi\tests\random.sps >> srfitest.log
-call isc lib\srfi\tests\random-access-lists.sps >> srfitest.log
-call isc lib\srfi\tests\rec-factorial.sps >> srfitest.log
-call isc lib\srfi\tests\records.sps >> srfitest.log
-call isc lib\srfi\tests\testing.sps >> srfitest.log
-call isc lib\srfi\tests\time.sps >> srfitest.log
+isc lib\srfi\tests\and-let_.sps >> srfitest.log
+isc lib\srfi\tests\compare-procedures.sps >> srfitest.log
+isc lib\srfi\tests\cut.sps >> srfitest.log
+isc lib\srfi\tests\eager-comprehensions.sps >> srfitest.log
+isc lib\srfi\tests\intermediate-format-strings.sps >> srfitest.log
+isc lib\srfi\tests\lightweight-testing.sps >> srfitest.log
+isc lib\srfi\tests\lists.sps >> srfitest.log
+isc lib\srfi\tests\multi-dimensional-arrays--arlib.sps >> srfitest.log
+isc lib\srfi\tests\multi-dimensional-arrays.sps >> srfitest.log
+isc lib\srfi\tests\os-environment-variables.sps >> srfitest.log
+isc lib\srfi\tests\procedure-arity.sps >> srfitest.log
+isc lib\srfi\tests\print-ascii.sps >> srfitest.log
+rem isc lib\srfi\tests\random-conftest.sps >> srfitest.log
+isc lib\srfi\tests\random.sps >> srfitest.log
+isc lib\srfi\tests\random-access-lists.sps >> srfitest.log
+isc lib\srfi\tests\rec-factorial.sps >> srfitest.log
+isc lib\srfi\tests\records.sps >> srfitest.log
+isc lib\srfi\tests\testing.sps >> srfitest.log
+isc lib\srfi\tests\time.sps >> srfitest.log
