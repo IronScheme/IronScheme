574ea53bfd0b829a68c9ea07c5fb97e546d9fb52
leppie <xacc.ide@gmail.com>
2013-06-17 18:54:07 +0000

    Exclude .cmd files for tar.xz file.
    git-svn-id: https://ironscheme.svn.codeplex.com/svn@101146 2327b42d-5241-43d6-9e2a-de5ac946f064

--- IronScheme/IronScheme.Console/stage.cmd
+++ IronScheme/IronScheme.Console/stage.cmd
@@ -8,8 +8,7 @@ copy IronScheme.Console32-v4.exe install-stage\IronScheme
 copy DEVELOPMENT.snk install-stage\IronScheme
 copy system-libraries.ss install-stage\IronScheme
 
-copy ngen-all.cmd install-stage\IronScheme
-copy un-ngen-all.cmd install-stage\IronScheme
+
 copy IronScheme.dll install-stage\IronScheme
 copy IronScheme.Web.Runtime.dll install-stage\IronScheme
 copy IronScheme.Remoting*.dll install-stage\IronScheme
@@ -29,7 +28,11 @@ rmdir /s /q install-stage\IronScheme\lib\srfi\tests
 rmdir /s /q install-stage\IronScheme\docs\notes
 
 cd install-stage
-..\zip -r -9 IronScheme-latest.zip IronScheme
 c:\MinGW\msys\1.0\bin\tar.exe -c -fIronScheme-latest.tar IronScheme
 ..\xz IronScheme-latest.tar
+
+copy ngen-all.cmd install-stage\IronScheme
+copy un-ngen-all.cmd install-stage\IronScheme
+
+..\zip -r -9 IronScheme-latest.zip IronScheme
 cd ..
