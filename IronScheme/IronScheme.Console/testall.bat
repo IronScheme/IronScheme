dc32198abae0f780d58184bd1f5148d962c9fdae
Llewellyn Pritchard <xacc.ide@gmail.com>
2013-06-28 14:49:11 +0000

    Delete dll's and pdb's after testing. (Todo make-test-stage)
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS02]$/IronScheme;C101358

--- IronScheme/IronScheme.Console/testall.bat
+++ IronScheme/IronScheme.Console/testall.bat
@@ -4,7 +4,7 @@ isc ironscheme-buildscript.sps
 peverify /nologo ironscheme.boot.dll
 @IF %ERRORLEVEL% NEQ 0 exit /b 1
 isc compile-system-libraries.sps
-isc compile-system-libraries.sps
+isc --show-loaded-libraries compile-system-libraries.sps > libraries.lst
 isc -debug ironscheme-buildscript.sps
 @IF %ERRORLEVEL% NEQ 0 exit /b 1
 isc -debug ironscheme-buildscript.sps
@@ -12,10 +12,15 @@ peverify /nologo /ignore=0x80131820 ironscheme.boot.dll
 @IF %ERRORLEVEL% NEQ 0 exit /b 1
 isc -debug compile-system-libraries.sps
 isc -debug compile-system-libraries.sps
+
+FOR /F %%F IN ('type libraries.lst') DO del %%~pnF.pdb
+
 isc ironscheme-buildscript.sps
 isc compile-system-libraries.sps
 call run-tests.bat
 call srfitest.bat
 isc lib\pfds\tests.scm > pfds.log
 echo (include "lib/minikanren/mktests.scm") | isc > mktest.log
+
+FOR /F %%f IN ('type libraries.lst') DO del %%f
 
