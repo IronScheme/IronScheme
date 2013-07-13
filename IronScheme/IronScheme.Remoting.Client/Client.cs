e84be08a96668bbdd98f46df6ca1d15d3c714c4d
Alexander M. Batishchev <abatishchev@gmail.com>
2013-07-12 23:11:02 -0700

    Update Client.cs
    Isn't it better to receive InvalidCast than return null and then get weird NullReference somewhere else?

--- IronScheme/IronScheme.Remoting.Client/Client.cs
+++ IronScheme/IronScheme.Remoting.Client/Client.cs
@@ -7,17 +7,19 @@
 
 using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Runtime.Remoting.Channels;
-using System.Runtime.Remoting.Channels.Ipc;
 using System.Diagnostics;
 using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Ipc;
+using System.Text;
 using System.Threading;
 
 namespace IronScheme.Remoting.Client
 {
   public static class ServiceManager
   {
+    private const string MutexName = "IronScheme";
+
     static ServiceManager()
     {
       BinaryClientFormatterSinkProvider sbs = new BinaryClientFormatterSinkProvider();
@@ -29,13 +31,13 @@ namespace IronScheme.Remoting.Client
 
     public static ISymbolBindingService GetSymbolBindingService()
     {
-      var sbs = RemotingServices.Connect(typeof(ISymbolBindingService), "ipc://IronScheme/SymbolBindingService") as ISymbolBindingService;
+      var sbs = (ISymbolBindingService)RemotingServices.Connect(typeof(ISymbolBindingService), "ipc://IronScheme/SymbolBindingService");
       return sbs;
     }
 
     public static IInteractionService GetInteractionService()
     {
-      var iis = RemotingServices.Connect(typeof(IInteractionService), "ipc://IronScheme/InteractionService") as IInteractionService;
+      var iis = (IInteractionService)RemotingServices.Connect(typeof(IInteractionService), "ipc://IronScheme/InteractionService");
       return iis;
     }
 
@@ -43,11 +45,12 @@ namespace IronScheme.Remoting.Client
     {
       get
       {
-        bool isnew;
-        var m = new Mutex(false, "IronScheme", out isnew);
-        m.ReleaseMutex();
-        m.Close();
-        return isnew;
+        bool isNew;
+        using (var m = new Mutex(false, MutexName, out isNew))
+        {
+          m.ReleaseMutex();
+        }
+        return isNew;
       }
     }
   }
