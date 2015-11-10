c964d7ee74bc6b0c27909503ec6f61e6dadbddff
Llewellyn Pritchard <xacc.ide@gmail.com>
2015-11-10 15:16:59 +0000

    Cleanup
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS02]$/IronScheme;C115528

--- IronScheme/IronScheme.Console/ilmerge.cmd
+++
@@ -1,1 +0,0 @@
-"C:\Program Files (x86)\Microsoft\ILMerge\ILMerge.exe"/ndebug /keyfile:DEVELOPMENT.snk /out:IronScheme.Console.exe IronScheme.Console.exe IronScheme.dll IronScheme.Closures.dll IronScheme.Remoting.Server.dll IronScheme.Scripting.dll Oyster.IntX.dll ironscheme.boot.dll 
--- IronScheme/IronScheme.Console/setup-xp.bat
+++
@@ -1,4 +0,0 @@
-xcopy *.dll websample\bin
-xcopy /e ironscheme websample\bin\ironscheme\
-xcopy /e lib websample\bin\lib\
-xcopy /e srfi websample\bin\srfi\
