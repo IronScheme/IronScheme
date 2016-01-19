7ca9de12e9fdb2b73ba1ed4f328760b0b5fb5ad8
Llewellyn Pritchard <xacc.ide@gmail.com>
2016-01-19 17:33:29 +0200

    Update appveyor stuff

--- appveyor/build.cmd
+++
@@ -1,3 +0,0 @@
-appveyor AddCompilationMessage "Starting build"
-msbuild "IronScheme\IronSchemeCI.sln" /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" /p:TargetFrameworkVersion=v2.0 /p:Configuration=Release
-appveyor AddCompilationMessage "Build completed"
--- appveyor/test.cmd
+++ appveyor/test.cmd
@@ -3,55 +3,55 @@ set PATH=%PATH%;"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 
 cd IronScheme\IronScheme.Console\bin\Release
 mklink isc.exe IronScheme.Console32.exe
 
-Add-AppveyorTest "Release bootstrap" -Outcome Running
+appveyor AddTest "Release bootstrap" -Outcome Running
 isc ironscheme-buildscript.sps
 @IF %ERRORLEVEL% NEQ 0 exit /b 1
 isc ironscheme-buildscript.sps
 peverify /nologo ironscheme.boot.dll
 @IF %ERRORLEVEL% NEQ 0 exit /b 1
-Update-AppveyorTest "Release bootstrap" -Outcome Passed
+appveyor UpdateTest "Release bootstrap" -Outcome Passed
 
-Add-AppveyorTest "Release libraries" -Outcome Running
+appveyor AddTest "Release libraries" -Outcome Running
 isc compile-system-libraries.sps
 ren lib lib.hide
 isc --show-loaded-libraries compile-system-libraries.sps > libraries.lst
 ren lib.hide lib
-Update-AppveyorTest "Release libraries" -Outcome Passed
+appveyor UpdateTest "Release libraries" -Outcome Passed
 
-Add-AppveyorTest "Debug bootstrap" -Outcome Running
+appveyor AddTest "Debug bootstrap" -Outcome Running
 isc -debug ironscheme-buildscript.sps
 @IF %ERRORLEVEL% NEQ 0 exit /b 1
 isc -debug ironscheme-buildscript.sps
 peverify /nologo /ignore=0x80131820 ironscheme.boot.dll
 @IF %ERRORLEVEL% NEQ 0 exit /b 1
-Update-AppveyorTest "Debug bootstrap" -Outcome Passed
+appveyor UpdateTest "Debug bootstrap" -Outcome Passed
 
-Add-AppveyorTest "Debug libraries" -Outcome Running
+appveyor AddTest "Debug libraries" -Outcome Running
 FOR /F %%f IN ('type libraries.lst') DO @del %%f
 isc -debug compile-system-libraries.sps
 ren lib lib.hide
 isc -debug compile-system-libraries.sps
 FOR /F %%F IN ('type libraries.lst') DO @del %%~pnF.pdb
 ren lib.hide lib
-Update-AppveyorTest "Debug libraries" -Outcome Passed
+appveyor UpdateTest "Debug libraries" -Outcome Passed
 
-Add-AppveyorTest "Release bootstrap for test" -Outcome Running
+appveyor AddTest "Release bootstrap for test" -Outcome Running
 isc ironscheme-buildscript.sps
 FOR /F %%f IN ('type libraries.lst') DO @del %%f
-Update-AppveyorTest "Release bootstrap for test" -Outcome Passed
+appveyor UpdateTest "Release bootstrap for test" -Outcome Passed
 
-Add-AppveyorTest "Release libraries for test" -Outcome Running
+appveyor AddTest "Release libraries for test" -Outcome Running
 isc compile-system-libraries.sps
-Update-AppveyorTest "Release libraries for test" -Outcome Passed
+appveyor UpdateTest "Release libraries for test" -Outcome Passed
 
-Add-AppveyorTest "R6RS" -Outcome Running
+appveyor AddTest "R6RS" -Outcome Running
 isc tests\r6rs\run.sps
 isc tests\trigtest.sps
 isc tests\clisp-number-tests.sps
 isc tests\fp-test.sps
-Update-AppveyorTest "R6RS" -Outcome Passed
+appveyor UpdateTest "R6RS" -Outcome Passed
 
-Add-AppveyorTest "SRFI" -Outcome Running
+appveyor AddTest "SRFI" -Outcome Running
 isc lib\srfi\tests\and-let_.sps
 isc lib\srfi\tests\compare-procedures.sps
 isc lib\srfi\tests\cut.sps
@@ -71,14 +71,14 @@ isc lib\srfi\tests\rec-factorial.sps
 isc lib\srfi\tests\records.sps
 isc lib\srfi\tests\testing.sps
 isc lib\srfi\tests\time.sps
-Update-AppveyorTest "SRFI" -Outcome Passed
+appveyor UpdateTest "SRFI" -Outcome Passed
 
-Add-AppveyorTest "PFDS" -Outcome Running
+appveyor AddTest "PFDS" -Outcome Running
 isc lib\pfds\tests.scm
-Update-AppveyorTest "PFDS" -Outcome Passed
+appveyor UpdateTest "PFDS" -Outcome Passed
 
-Add-AppveyorTest "MiniKanren" -Outcome Running
+appveyor AddTest "MiniKanren" -Outcome Running
 echo (include "lib/minikanren/mktests.scm") | isc
-Update-AppveyorTest "MiniKanren" -Outcome Passed
+appveyor UpdateTest "MiniKanren" -Outcome Passed
 
 FOR /F %%f IN ('type libraries.lst') DO @del %%f
