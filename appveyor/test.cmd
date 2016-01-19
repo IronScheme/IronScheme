07e843eaf8a3fbbed31d11c0eb1a5fa9d3d28b04
Llewellyn Pritchard <xacc.ide@gmail.com>
2016-01-19 17:39:10 +0200

    Update appveyor again due to lying docs!

--- appveyor/test.cmd
+++ appveyor/test.cmd
@@ -3,55 +3,55 @@ set PATH=%PATH%;"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 
 cd IronScheme\IronScheme.Console\bin\Release
 mklink isc.exe IronScheme.Console32.exe
 
-appveyor AddTest "Release bootstrap" -Outcome Running
+REM appveyor AddTest "Release bootstrap" -Outcome Running
 isc ironscheme-buildscript.sps
 @IF %ERRORLEVEL% NEQ 0 exit /b 1
 isc ironscheme-buildscript.sps
 peverify /nologo ironscheme.boot.dll
 @IF %ERRORLEVEL% NEQ 0 exit /b 1
-appveyor UpdateTest "Release bootstrap" -Outcome Passed
+REM appveyor UpdateTest "Release bootstrap" -Outcome Passed
 
-appveyor AddTest "Release libraries" -Outcome Running
+REM appveyor AddTest "Release libraries" -Outcome Running
 isc compile-system-libraries.sps
 ren lib lib.hide
 isc --show-loaded-libraries compile-system-libraries.sps > libraries.lst
 ren lib.hide lib
-appveyor UpdateTest "Release libraries" -Outcome Passed
+REM appveyor UpdateTest "Release libraries" -Outcome Passed
 
-appveyor AddTest "Debug bootstrap" -Outcome Running
+REM appveyor AddTest "Debug bootstrap" -Outcome Running
 isc -debug ironscheme-buildscript.sps
 @IF %ERRORLEVEL% NEQ 0 exit /b 1
 isc -debug ironscheme-buildscript.sps
 peverify /nologo /ignore=0x80131820 ironscheme.boot.dll
 @IF %ERRORLEVEL% NEQ 0 exit /b 1
-appveyor UpdateTest "Debug bootstrap" -Outcome Passed
+REM appveyor UpdateTest "Debug bootstrap" -Outcome Passed
 
-appveyor AddTest "Debug libraries" -Outcome Running
+REM appveyor AddTest "Debug libraries" -Outcome Running
 FOR /F %%f IN ('type libraries.lst') DO @del %%f
 isc -debug compile-system-libraries.sps
 ren lib lib.hide
 isc -debug compile-system-libraries.sps
 FOR /F %%F IN ('type libraries.lst') DO @del %%~pnF.pdb
 ren lib.hide lib
-appveyor UpdateTest "Debug libraries" -Outcome Passed
+REM appveyor UpdateTest "Debug libraries" -Outcome Passed
 
-appveyor AddTest "Release bootstrap for test" -Outcome Running
+REM appveyor AddTest "Release bootstrap for test" -Outcome Running
 isc ironscheme-buildscript.sps
 FOR /F %%f IN ('type libraries.lst') DO @del %%f
-appveyor UpdateTest "Release bootstrap for test" -Outcome Passed
+REM appveyor UpdateTest "Release bootstrap for test" -Outcome Passed
 
-appveyor AddTest "Release libraries for test" -Outcome Running
+REM appveyor AddTest "Release libraries for test" -Outcome Running
 isc compile-system-libraries.sps
-appveyor UpdateTest "Release libraries for test" -Outcome Passed
+REM appveyor UpdateTest "Release libraries for test" -Outcome Passed
 
-appveyor AddTest "R6RS" -Outcome Running
+REM appveyor AddTest "R6RS" -Outcome Running
 isc tests\r6rs\run.sps
 isc tests\trigtest.sps
 isc tests\clisp-number-tests.sps
 isc tests\fp-test.sps
-appveyor UpdateTest "R6RS" -Outcome Passed
+REM appveyor UpdateTest "R6RS" -Outcome Passed
 
-appveyor AddTest "SRFI" -Outcome Running
+REM appveyor AddTest "SRFI" -Outcome Running
 isc lib\srfi\tests\and-let_.sps
 isc lib\srfi\tests\compare-procedures.sps
 isc lib\srfi\tests\cut.sps
@@ -71,14 +71,14 @@ isc lib\srfi\tests\rec-factorial.sps
 isc lib\srfi\tests\records.sps
 isc lib\srfi\tests\testing.sps
 isc lib\srfi\tests\time.sps
-appveyor UpdateTest "SRFI" -Outcome Passed
+REM appveyor UpdateTest "SRFI" -Outcome Passed
 
-appveyor AddTest "PFDS" -Outcome Running
+REM appveyor AddTest "PFDS" -Outcome Running
 isc lib\pfds\tests.scm
-appveyor UpdateTest "PFDS" -Outcome Passed
+REM appveyor UpdateTest "PFDS" -Outcome Passed
 
-appveyor AddTest "MiniKanren" -Outcome Running
+REM appveyor AddTest "MiniKanren" -Outcome Running
 echo (include "lib/minikanren/mktests.scm") | isc
-appveyor UpdateTest "MiniKanren" -Outcome Passed
+REM appveyor UpdateTest "MiniKanren" -Outcome Passed
 
 FOR /F %%f IN ('type libraries.lst') DO @del %%f
