e14f0fb68ffaab16aebc3fbf711ad9235db5d73c
Llewellyn Pritchard <xacc.ide@gmail.com>
2016-01-20 18:43:09 +0200

    Add Nunit tests to automate scripted tests

---
+++ IronScheme/IronScheme.Tests/OtherTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using System.Diagnostics;
+
+namespace IronScheme.Tests
+{
+  public class OtherTests : TestRunner
+  {
+    //[Test]
+    public void PFDS()
+    {
+      var r = RunTest(@"lib\pfds\tests.scm");
+      Debugger.Break();
+      Console.WriteLine(r.Output);
+      Console.WriteLine("Expected 8 failed tests.");
+      Assert.True(r.Output.Contains("8 of 8970 tests failed."));
+    }
+
+    //[Test]
+    public void MiniKanren()
+    {
+
+    }
+  }
+}
---
+++ IronScheme/IronScheme.Tests/TestRunner.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using System.Diagnostics;
+
+namespace IronScheme.Tests
+{
+  [TestFixture]
+  public abstract class TestRunner
+  {
+    protected class TestResult
+    {
+      public string Output;
+      public int ExitCode;
+    }
+
+    protected TestResult RunTest(string filename)
+    {
+      var p = new Process
+      {
+        StartInfo = new ProcessStartInfo
+        {
+          FileName = "IronScheme.Console32.exe",
+          RedirectStandardOutput = true,
+          RedirectStandardError = true,
+          RedirectStandardInput = true,
+          UseShellExecute = false,
+          CreateNoWindow = true,
+          Arguments = filename
+        }
+      };
+
+      p.Start();
+
+      var exited = p.WaitForExit(30000);
+
+      if (!exited)
+      {
+        p.Kill();
+      }
+
+      var r = new TestResult
+      {
+        ExitCode = p.ExitCode,
+        Output = p.StandardOutput.ReadToEnd()
+      };
+
+      Assert.AreEqual(0, p.ExitCode);
+
+      return r;
+    }
+  }
+}
