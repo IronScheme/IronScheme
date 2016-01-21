80cd0f8b831ff9ff20285dde87fb0fff4adf1d89
Llewellyn Pritchard <xacc.ide@gmail.com>
2016-01-21 12:38:58 +0200

    Update ci setup

--- appveyor/setup.cmd
+++ appveyor/setup.cmd
@@ -1,1 +1,6 @@
 cinst ilmerge
+cinst zip
+curl -o xz.zip http://tukaani.org/xz/xz-5.2.1-windows.zip
+unzip -o xz.zip -d xz bin_i686-sse2/xz.exe
+copy xz\bin_i686-sse2\xz.exe "C:\Program Files\Git\usr\bin\"
+set PATH=%PATH%;C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\;
