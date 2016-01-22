6827d5b1b618853a658534b65978efd36559567b
Llewellyn Pritchard <xacc.ide@gmail.com>
2016-01-22 12:08:03 +0200

    Let's see if it gets picked up now

--- IronScheme/IronScheme/Properties/AssemblyInfo.cs
+++ IronScheme/IronScheme/Properties/AssemblyInfo.cs
@@ -10,6 +10,7 @@ using System.Security.Permissions;
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
 
 // these are for when Oyster.IntX gets merged. ILMerge seems to forget about them.
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
