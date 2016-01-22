72a51260270865c842b92e95f68f3b140c1fcf9f
Llewellyn Pritchard <xacc.ide@gmail.com>
2016-01-22 22:06:00 +0200

    Update copyright info

--- IronScheme/IronScheme.Console/playground/linq3.sls
+++ IronScheme/IronScheme.Console/playground/linq3.sls
@@ -1,6 +1,6 @@
 ﻿#!r6rs
 #| License
-Copyright (c) 2007-2015 Llewellyn Pritchard
+Copyright (c) 2007-2016 Llewellyn Pritchard
 All rights reserved.
 This source code is subject to terms and conditions of the BSD License.
 See docs/license.txt. |#
--- IronScheme/IronScheme/Compiler/Optimizer.FlattenBodies.cs
+++ IronScheme/IronScheme/Compiler/Optimizer.FlattenBodies.cs
@@ -1,5 +1,5 @@
 ﻿#region License
-/* Copyright (c) 2007-2015 Llewellyn Pritchard
+/* Copyright (c) 2007-2016 Llewellyn Pritchard
  * All rights reserved.
  * This source code is subject to terms and conditions of the BSD License.
  * See docs/license.txt. */
