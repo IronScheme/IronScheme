a79634c3335e93c7f6efa1e28dbf33feb2188a7e
Llewellyn Pritchard <xacc.ide@gmail.com>
2016-01-27 14:06:48 +0200

    Update test.cmd to check for peverify and nunit
    [skip ci]

--- IronScheme/test.cmd
+++ IronScheme/test.cmd
@@ -7,8 +7,18 @@ set TESTS=Release,Debug,Setup,Conformance,SRFI,Other,Teardown
 rem set this to anything if you want dont want verbose output
 set QUIET=1
 
-rem set the path to nunit
-set PATH=%PATH%;%USERPROFILE%\Downloads\NUnit-2.6.4\NUnit-2.6.4\bin\;
+rem set nunit bin directory
+set NUNIT_PATH=%USERPROFILE%\Downloads\NUnit-2.6.4\NUnit-2.6.4\bin\
+
+rem setup path
+set PATH=%PATH%;%NUNIT_PATH%;
+
+rem checks
+where peverify >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 goto no_peverify
+where nunit-console-x86 >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 goto no_nunit
+
 
 set NUNIT=call :runtest
 
@@ -27,3 +37,12 @@ nunit-console-x86.exe /nologo /labels /framework:net-2.0 ^
 IronScheme.Tests.dll /run:IronScheme.Tests.%1
 @echo off 
 goto :eof
+
+:no_peverify
+echo Error: PEVerify not found in PATH
+exit /b 1
+
+:no_nunit
+echo Error: NUnit path not set correctly
+exit /b 1
+
