9bd7ccaf2273fdaeb0f574a8d1b8ba281639e721
Llewellyn Pritchard <xacc.ide@gmail.com>
2016-02-05 23:07:58 +0200

    Fix path

--- ci-artefact.cmd
+++ ci-artefact.cmd
@@ -1,3 +1,5 @@
-@call IronScheme\package.cmd
-@ren IronScheme\bin\IronScheme-latest.zip IronScheme-%APPVEYOR_BUILD_VERSION%-%SHA%.zip
-@ren IronScheme\bin\IronScheme-latest.tar.xz IronScheme-%APPVEYOR_BUILD_VERSION%-%SHA%.tar.xz
+@cd IronScheme
+@call package.cmd
+@ren bin\IronScheme-latest.zip ..\IronScheme-%APPVEYOR_BUILD_VERSION%-%SHA%.zip
+@ren bin\IronScheme-latest.tar.xz ..\IronScheme-%APPVEYOR_BUILD_VERSION%-%SHA%.tar.xz
+@cd ..
