8e9bbe2e4fd8aa15fa8a44a022febf1388514af9
Llewellyn Pritchard <xacc.ide@gmail.com>
2016-02-10 18:53:34 +0200

    Create a debug artefact

--- ci-artefact.cmd
+++ ci-artefact.cmd
@@ -3,7 +3,8 @@ cd IronScheme
 call package.cmd
 cd bin
 ren IronScheme-latest.zip IronScheme-%APPVEYOR_BUILD_VERSION%-%SHA%.zip
+ren IronScheme-latest-DEBUG.zip IronScheme-%APPVEYOR_BUILD_VERSION%-%SHA%-DEBUG.zip
 ren IronScheme-latest.tar.xz IronScheme-%APPVEYOR_BUILD_VERSION%-%SHA%.tar.xz
-copy IronScheme-%APPVEYOR_BUILD_VERSION%-%SHA%.* %APPVEYOR_BUILD_FOLDER%
+copy IronScheme-%APPVEYOR_BUILD_VERSION%-%SHA%*.* %APPVEYOR_BUILD_FOLDER%
 cd %APPVEYOR_BUILD_FOLDER%
 @echo off
