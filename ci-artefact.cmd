f540afcffcc8abfe0a789cb33ef7e56aba5c4162
Llewellyn Pritchard <xacc.ide@gmail.com>
2018-10-25 07:37:42 +0200

    Update ci-artefact.cmd

--- ci-artefact.cmd
+++ ci-artefact.cmd
@@ -4,7 +4,7 @@ call package.cmd
 cd bin
 ren IronScheme-latest.zip IronScheme-%APPVEYOR_BUILD_VERSION%-%SHA%.zip
 ren IronScheme-latest-DEBUG.zip IronScheme-%APPVEYOR_BUILD_VERSION%-%SHA%-DEBUG.zip
-ren IronScheme-latest.tar.xz IronScheme-%APPVEYOR_BUILD_VERSION%-%SHA%.tar.xz
+rem ren IronScheme-latest.tar.xz IronScheme-%APPVEYOR_BUILD_VERSION%-%SHA%.tar.xz
 copy IronScheme-%APPVEYOR_BUILD_VERSION%-%SHA%*.* %APPVEYOR_BUILD_FOLDER%
 cd %APPVEYOR_BUILD_FOLDER%
 @echo off
