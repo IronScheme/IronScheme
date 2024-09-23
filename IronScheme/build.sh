4b6f00f326f009551a5c5b0d6dfd1e86793dd792
Llewellyn Pritchard <xacc.ide@gmail.com>
2024-09-23 20:12:27 +0200

    Better build output

--- IronScheme/build.sh
+++ IronScheme/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-dotnet build -c Release -p:NoWarn="NETSDK1138%3bNU1702%3bNU1902%3bNU1903%3bNU1701%3bCS3021%3bSYSLIB0050"
+dotnet build -v:m -tl:off -c Release -p:NoWarn="NETSDK1138%3bNU1702%3bNU1902%3bNU1903%3bNU1701%3bCS3021%3bSYSLIB0050"
