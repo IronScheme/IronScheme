32fef777fdb357d7eef89d664106746c0ccddd80
Llewellyn Pritchard <xacc.ide@gmail.com>
2024-09-23 19:45:43 +0200

    Clean up script

--- IronScheme/build.sh
+++ IronScheme/build.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-dotnet build -v:n -tl:off -m:1 -c Release -p:NoWarn="NETSDK1138%3bNU1702%3bNU1902%3bNU1903%3bNU1701%3bCS3021%3bSYSLIB0050"
+dotnet build -c Release -p:NoWarn="NETSDK1138%3bNU1702%3bNU1902%3bNU1903%3bNU1701%3bCS3021%3bSYSLIB0050"
