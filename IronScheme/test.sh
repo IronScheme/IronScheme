054e5e507b8cd5a43feeca49ab244cd37fa7d874
Llewellyn Pritchard <xacc.ide@gmail.com>
2024-10-01 15:46:04 +0200

    Take 10

--- IronScheme/test.sh
+++ IronScheme/test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-dotnet build --disable-build-servers --no-incremental -restore -v:m -tl:off --force -c Release -f net9.0 IronScheme.Tests/IronScheme.Tests.csproj
+dotnet clean IronScheme.Tests/IronScheme.Tests.csproj -tl:off -c Release 
+dotnet build IronScheme.Tests/IronScheme.Tests.csproj -tl:off -c Release -f net9.0 --disable-build-servers --no-incremental --force
 
 cd IronScheme.Console/bin/Release/net9.0/
 export ISWD=$PWD
 export TESTCORE=1
 
-dotnet test -v n ../../../../IronScheme.Tests/bin/Release/IronScheme.Tests.dll -- NUnit.DefaultTestNamePattern="{c}.{m}" NUnit.PreFilter=true NUnit.StopOnError=true
+dotnet test -v d ../../../../IronScheme.Tests/bin/Release/IronScheme.Tests.dll -- NUnit.DefaultTestNamePattern="{c}.{m}" NUnit.PreFilter=true NUnit.StopOnError=true
 cd ../../../..
