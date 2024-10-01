693f0bb58c1fb4a570bc2f24f9c108f6ea5dde6d
Llewellyn Pritchard <xacc.ide@gmail.com>
2024-10-01 14:58:46 +0200

    Improve scripts

--- IronScheme/build.sh
+++ IronScheme/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-dotnet build -tl:off -c Release ../IronScheme.BuildTools/IronScheme.Build/IronScheme.Build.csproj
-dotnet build -tl:off -c Release -p:TargetFramework=net9.0 ../IronScheme.BuildTools/Setup/Setup.csproj
-dotnet build -tl:off -c Release -p:IronSchemeBuildToolsTargetFramework=net9.0 $*
+dotnet build --disable-build-servers -tl:off -c Release ../IronScheme.BuildTools/IronScheme.Build/IronScheme.Build.csproj
+dotnet build --disable-build-servers -tl:off -c Release -p:TargetFramework=net9.0 ../IronScheme.BuildTools/Setup/Setup.csproj
+dotnet build --disable-build-servers -tl:off -c Release -p:IronSchemeBuildToolsTargetFramework=net9.0 $*
