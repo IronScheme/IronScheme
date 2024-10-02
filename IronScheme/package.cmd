c693ff4639cf83b15eb30d5322fedeb6d8dc806f
Llewellyn Pritchard <xacc.ide@gmail.com>
2024-10-02 16:39:24 +0200

    xplat exe's

--- IronScheme/package.cmd
+++ IronScheme/package.cmd
@@ -1,4 +1,8 @@
 
 dotnet build --disable-build-servers -tl:off -c Release ../IronScheme.BuildTools/IronScheme.Build/IronScheme.Build.csproj
+
+dotnet build --disable-build-servers IronScheme.Console/IronScheme.Console.csproj -f net9.0 -r osx-x64 -tl:off -v:n -c Release
+dotnet build --disable-build-servers IronScheme.Console/IronScheme.Console.csproj -f net9.0 -r linux-x64 -tl:off -v:n -c Release
+
 dotnet build --disable-build-servers -tl:off -c Release -t:Package %*
 exit /B %ERRORLEVEL%
