cbf238765a5d48428c2b5e93ae3060c091648977
Llewellyn Pritchard <xacc.ide@gmail.com>
2024-10-16 14:06:32 +0200

    Coverage

--- IronScheme/cov-run.cmd
+++ IronScheme/cov-run.cmd
@@ -3,6 +3,7 @@ setlocal
 
 cd IronScheme.Console\bin\Release\net20
 del /q IronScheme.FrameworkPAL.*
+del /q IronScheme.Console.*
 
 copy ..\netcoreapp2.1\IronScheme.ConsoleCore.dll .
 copy ..\net9.0\IronScheme.ConsoleCore.runtimeconfig.json .
@@ -12,7 +13,5 @@ echo (compile-system-libraries) | dotnet IronScheme.ConsoleCore.dll -debug
 SET ISWD=%CD%
 SET TESTCORE=1
 
-SET LOGGER=--logger GitHubActions;summary.includePassedTests=true;summary.includeSkippedTests=true
-
-dotnet test -v n ..\..\..\..\IronScheme.Tests\bin\Release\IronScheme.Tests.dll %LOGGER% --filter "Category=Conformance|SRFI|Other" -- NUnit.DefaultTestNamePattern="{c}.{m}" NUnit.PreFilter=true NUnit.StopOnError=false
-dotnet test -v n ..\..\..\..\IronScheme.Tests\bin\Release\IronScheme.Tests.dll %LOGGER% --filter "Bootstrap_Debug" -- NUnit.DefaultTestNamePattern="{c}.{m}" NUnit.PreFilter=true NUnit.StopOnError=false
+dotnet test -v n ..\..\..\..\IronScheme.Tests\bin\Release\IronScheme.Tests.dll --filter "Category=Conformance|SRFI|Other" -- NUnit.DefaultTestNamePattern="{c}.{m}" NUnit.PreFilter=true NUnit.StopOnError=false
+rem dotnet test -v n ..\..\..\..\IronScheme.Tests\bin\Release\IronScheme.Tests.dll --filter "Bootstrap_Debug" -- NUnit.DefaultTestNamePattern="{c}.{m}" NUnit.PreFilter=true NUnit.StopOnError=false
--- IronScheme/coverage.cmd
+++ IronScheme/coverage.cmd
@@ -4,4 +4,10 @@ dotnet tool install -g dotnet-coverage --prerelease >nul
 dotnet tool install -g dotnet-ilverify --prerelease >nul
 rem dotnet tool install -g dotnet-reportgenerator-globaltool >nul
 
+del /q output.cobertura.xml
+del /q output.coverage
+
 dotnet-coverage collect "cov-run.cmd"
+
+rem dotnet-coverage merge output.coverage -f cobertura
+rem reportgenerator -reports:output.cobertura.xml -targetdir:cov
