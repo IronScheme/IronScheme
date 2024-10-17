44988a0e113d3c247985a633e3cd016555173263
Llewellyn Pritchard <xacc.ide@gmail.com>
2024-10-17 18:53:51 +0200

    New Nuget project

--- IronScheme/ci-artefact.cmd
+++ IronScheme/ci-artefact.cmd
@@ -5,24 +5,21 @@ if "%APPVEYOR_BUILD_VERSION%"=="" set APPVEYOR_BUILD_VERSION=0.0.0
 if "%SHA%"=="" set SHA=000000
 if "%APPVEYOR_BUILD_FOLDER%"=="" set APPVEYOR_BUILD_FOLDER=%CD%
 
-cd bin
-ren IronScheme-latest.zip IronScheme-%APPVEYOR_BUILD_VERSION%-%SHA%.zip
-rem ren IronScheme-latest-DEBUG.zip IronScheme-%APPVEYOR_BUILD_VERSION%-%SHA%-DEBUG.zip
+ren bin\IronScheme-latest.zip IronScheme-%APPVEYOR_BUILD_VERSION%-%SHA%.zip
 
-set BINDIR=%CD%
-
-pushd ..\IronScheme.Console\bin\Release\net20\install-stage
-cd IronScheme
+pushd IronScheme.Console\bin\Release\net20\install-stage\IronScheme
 echo (import (nuget)) (fuck) | IronScheme.Console32-v2
-cd ..
-copy /y ..\..\..\..\IronScheme.*.nuspec .
-nuget pack IronScheme.Core.nuspec -properties version=%APPVEYOR_BUILD_VERSION%;sha=%SHA%
-nuget pack IronScheme.Tool.nuspec -properties version=%APPVEYOR_BUILD_VERSION%;sha=%SHA%
-copy /y IronScheme*.*nupkg %BINDIR%
-
 popd
 
-if "%APPVEYOR_BUILD_FOLDER%\bin"=="%CD%" exit /b %ERRORLEVEL%
+call nuget.cmd
+
+echo on
+
+pushd IronScheme.Console\bin\Release\net20\install-stage\IronScheme
+echo (import (nuget)) (unfuck) | IronScheme.Console32-v2
+popd
+
+if "%APPVEYOR_BUILD_FOLDER%"=="%CD%" exit /b %ERRORLEVEL%
 
 copy IronScheme-%APPVEYOR_BUILD_VERSION%-%SHA%*.zip %APPVEYOR_BUILD_FOLDER%
 copy IronScheme.*.%APPVEYOR_BUILD_VERSION%*.*nupkg %APPVEYOR_BUILD_FOLDER%
