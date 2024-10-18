012c71aa431bdf93a8b869066ec40f7216b45b3d
Llewellyn Pritchard <xacc.ide@gmail.com>
2024-10-18 20:45:41 +0200

    Try test netcoreapp2.1 on non-Windows too

--- IronScheme/test-coreapp2.1.sh
+++ IronScheme/test-coreapp2.1.sh
@@ -6,10 +6,11 @@ dotnet tool install -g dotnet-ilverify --prerelease
 dotnet build IronScheme.Tests/IronScheme.Tests.csproj -restore -m -tl:off -c Release -p:TargetFramework=net9.0 --no-incremental --force -v:q -clp:NoSummary
 dotnet build IronScheme.Tests/IronScheme.Tests.csproj -restore -m -tl:off -c Release -p:TargetFramework=net9.0 --no-incremental --force -v:q -clp:NoSummary
 
-cd IronScheme.Console/bin/Release/net9.0/
+cd IronScheme.Console/bin/Release/netcoreapp2.1/
 export ISWD=$PWD
 export TESTCORE=1
 
+FILTER='--filter "Category=Conformance|SRFI|Other"'
 LOGGER='--logger GitHubActions;summary.includePassedTests=true;summary.includeSkippedTests=true'
 
-dotnet test -v d ../../../../IronScheme.Tests/bin/Release/IronScheme.Tests.dll $LOGGER $* -- NUnit.DefaultTestNamePattern="{c}.{m}" NUnit.PreFilter=true NUnit.StopOnError=false
+dotnet test -v d ../../../../IronScheme.Tests/bin/Release/IronScheme.Tests.dll $LOGGER $FILTER $* -- NUnit.DefaultTestNamePattern="{c}.{m}" NUnit.PreFilter=true NUnit.StopOnError=false
