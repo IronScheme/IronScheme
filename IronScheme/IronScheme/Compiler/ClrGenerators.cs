65e75d3fae0247922a5fa4b9500d94f5a179223c
Llewellyn Pritchard <xacc.ide@gmail.com>
2025-12-05 16:14:19 +0200

    Support for array types with `#(type)`

--- IronScheme/IronScheme/Compiler/ClrGenerators.cs
+++ IronScheme/IronScheme/Compiler/ClrGenerators.cs
@@ -153,6 +153,11 @@ namespace IronScheme.Compiler
         var ft = mt.MakeGenericType(ga.ToArray());
         return ft;
       }
+      else if (type is object[] arr && arr.Length == 1)
+      {
+        var et = ReadType(arr[0]);
+        return et?.MakeArrayType();
+      }
       else if (type is SymbolId)
       {
         var st = SymbolTable.IdToString((SymbolId) type);
