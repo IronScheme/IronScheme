0b07033f14be3817a48350547ec25f65f6ec82ed
Llewellyn Pritchard <xacc.ide@gmail.com>
2025-12-15 20:12:22 +0200

    Add nuget with libs too

--- IronScheme/nuget.cmd
+++ IronScheme/nuget.cmd
@@ -1,13 +1,17 @@
 @echo off
 setlocal
 
+pushd IronScheme.Nuget
+dotnet pack -tl:off -p:PackageName=Core %*
+dotnet pack -tl:off %*
+popd
+
 pushd IronScheme.Console\bin\Release\net20\install-stage\IronScheme
 echo (import (nuget)) (fuck) | IronScheme.Console32-v4
 popd
 
 pushd IronScheme.Nuget
 dotnet pack -tl:off -p:PackageName=Tool %*
-dotnet pack -tl:off -p:PackageName=Core %*
 popd
 
 pushd IronScheme.Console\bin\Release\net20\install-stage\IronScheme
