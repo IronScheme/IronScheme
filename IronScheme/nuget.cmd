2b1c75516031bf30d9e5747b4d2f01b259b3db71
Llewellyn Pritchard <xacc.ide@gmail.com>
2025-12-17 11:48:02 +0200

    Probably going to break nuget

--- IronScheme/nuget.cmd
+++ IronScheme/nuget.cmd
@@ -2,8 +2,8 @@
 setlocal
 
 pushd IronScheme.Console\bin\Release\net20\install-stage
-mkdir tmp\contentFiles\any\any\lib\
-mklink /j tmp\contentFiles\any\any\lib\ironscheme IronScheme\lib\ironscheme\
+mkdir tmp\contentFiles\any\any\
+mklink /j tmp\contentFiles\any\any\lib IronScheme\lib
 popd
 
 pushd IronScheme.Nuget
