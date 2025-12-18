2f7b6140c74479ce8d8cbe7cad0e406030d8cde4
Llewellyn Pritchard <xacc.ide@gmail.com>
2025-12-18 11:03:49 +0200

    Zip in a zip because

--- IronScheme/nuget.cmd
+++ IronScheme/nuget.cmd
@@ -1,21 +1,13 @@
 @echo off
 setlocal
 
-pushd IronScheme.Console\bin\Release\net20\install-stage\IronScheme
-echo (import (nuget)) (fuck) | IronScheme.Console32-v4
-popd
-
-pushd IronScheme.Console\bin\Release\net20\install-stage
-mkdir tmp\build\
-mkdir tmp\contentFiles\any\any\
-mklink /j tmp\contentFiles\any\any\lib IronScheme\lib\
-mklink /h tmp\contentFiles\any\any\nuget.sls IronScheme\nuget.sls
-popd
-
 pushd IronScheme.Nuget
-copy /y IronScheme.targets ..\IronScheme.Console\bin\Release\net20\install-stage\tmp\build\
 dotnet pack -tl:off -p:PackageName=Core %*
 dotnet pack -tl:off %*
+popd
+
+pushd IronScheme.Console\bin\Release\net20\install-stage\IronScheme
+echo (import (nuget)) (fuck) | IronScheme.Console32-v4
 popd
 
 pushd IronScheme.Nuget
